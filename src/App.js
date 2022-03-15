37b55434c7359ce5b5bfdec7ef5f5cd6776be034
Linus Foxell <linusfoxell@hotmail.com>
2022-03-15 15:26:32 +0100

    pkgs installed - components added - routing added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Products from "./components/Products";
+import Product from "./components/Product";
+import Cart from "./components/Cart";
+import ErrorPage from "./components/ErrorPage";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/products" exact element={<Products />} />
+          <Route path="/product/:id" exact element={<Product />} />
+          <Route path="/cart" exact element={<Cart />} />
+          <Route path="*" elemen={<ErrorPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
