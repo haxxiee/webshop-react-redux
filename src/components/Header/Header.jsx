4d729e743ac124488502b0bcf26ef1ac55e8e27d
Linus Foxell <linusfoxell@hotmail.com>
2022-03-15 19:54:45 +0100

    sass added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "./Header.module.scss";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className={styles.header}>
+      <h1>Header</h1>
+    </div>
+  );
 };
 
 export default Header;
