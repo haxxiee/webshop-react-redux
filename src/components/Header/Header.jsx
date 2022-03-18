7d5d84ed48dafcbe9bf39b1818eb7e553872ea8d
Linus Foxell <linusfoxell@hotmail.com>
2022-03-18 14:57:19 +0100

    css + animations added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,9 @@ const Header = () => {
     <div className={styles.header__wrapper}>
       <div className={styles.header}>
         <Link to={"/"}>
-          <div className={styles.logo}>Amaxon</div>
+          <div className={styles.logo}>
+            Ama<span>x</span>on
+          </div>
         </Link>
         <nav>
           <ul>
